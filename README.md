c7c256425b9febcc3ce640a963b3354f4d4d1a75
JetBrain <nazir.hussain@branex.com>
2022-08-04 15:33:01 +0500

    Login functionality

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Document to create auth scoffolding [Docs](https://laravelarticle.com/laravel-8-
 ## Create Fake Record of the Model
 
 User::factory()->count(5)->create()
+
+## Auth trait 
+
+https://laravel.com/api/6.x/Illuminate/Foundation/Auth/AuthenticatesUsers.html
+
+https://www.codewithharry.com/blogpost/mongodb-cheatsheet/
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
+
+    protected $loginAttempts = 0;
+
     /*
     |--------------------------------------------------------------------------
     | Login Controller
@@ -36,5 +41,42 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login (Request $request) {
+
+        $request->validate([
+            'username'  => 'required',
+            'password'  => 'required',
+        ]);
+
+        $credentials = $request->only('username', 'password');
+
+        $remember = ($request->has('remember') && $request->get('remember') === "on") ? true: false;
+
+        if (Auth::attempt($credentials, $remember)) {
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect()->back()
+            ->withInput()
+            ->withErrors([
+                'login' => 'These credentials do not match our records.',
+            ]);
+    }
+
+    protected function incrementLoginAttempts(Request $request) {
+
+        $this->loginAttempts++;
+    }
+
+    protected function hasTooManyLoginAttempts(Request $request) {
+
+        return ($this->loginAttempts > 0) ? true: false;
+    }
+
+    protected function clearLoginAttempts(Request $request) {
+
+        $this->loginAttempts = 0;
     }
 }
